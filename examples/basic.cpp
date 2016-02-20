85e70fddcf1a82dacda7ec985dca33f66996e472
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-02-20 16:30:16 +0900

    fix space

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -6,29 +6,29 @@ namespace plt = matplotlibcpp;
 
 int main() 
 {
-	// Prepare data.
-	int n = 5000;
-	std::vector<double> x(n), y(n), z(n), w(n,2);
-	for(int i=0; i<n; ++i) {
-		x.at(i) = i*i;
-		y.at(i) = sin(2*M_PI*i/360.0);
-		z.at(i) = log(i);
-	}
+  // Prepare data.
+  int n = 5000;
+  std::vector<double> x(n), y(n), z(n), w(n,2);
+  for(int i=0; i<n; ++i) {
+	x.at(i) = i*i;
+	y.at(i) = sin(2*M_PI*i/360.0);
+	z.at(i) = log(i);
+  }
 
-	// Plot line from given x and y data. Color is selected automatically.
-	plt::plot(x, y);
-	// Plot a red dashed line from given x and y data.
-	plt::plot(x, w,"r--");
-	// Plot a line whose name will show up as "log(x)" in the legend.
-	plt::named_plot("log(x)", x, z);
+  // Plot line from given x and y data. Color is selected automatically.
+  plt::plot(x, y);
+  // Plot a red dashed line from given x and y data.
+  plt::plot(x, w,"r--");
+  // Plot a line whose name will show up as "log(x)" in the legend.
+  plt::named_plot("log(x)", x, z);
 
-	// Set x-axis to interval [0,1000000]
-	plt::xlim(0, 1000*1000);
+  // Set x-axis to interval [0,1000000]
+  plt::xlim(0, 1000*1000);
 
   // Add graph title
   plt::title("sample figure");
-	// Enable legend.
-	plt::legend();
-	// save figure
+  // Enable legend.
+  plt::legend();
+  // save figure
   plt::save("./basic.png");
 }
