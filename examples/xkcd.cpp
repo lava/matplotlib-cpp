fb4cdf929bb4e578a46601b8110c6e07700e4b01
tirtadwipam <8468685+tirtadwipam@users.noreply.github.com>
2017-12-06 20:01:38 +0700

    Support for stem and xkcd (#43)
    * Add support for stem and xkcd
    * Add README for xkcd

---
+++ examples/xkcd.cpp
@@ -0,0 +1,21 @@
+#include "../matplotlibcpp.h"
+#include <vector>
+#include <cmath>
+
+namespace plt = matplotlibcpp;
+
+int main() {
+    std::vector<double> t(1000);
+    std::vector<double> x(t.size());
+
+    for(size_t i = 0; i < t.size(); i++) {
+        t[i] = i / 100.0;
+        x[i] = sin(2.0 * M_PI * 1.0 * t[i]);
+    }
+
+    plt::xkcd();
+    plt::plot(t, x);
+    plt::title("AN ORDINARY SIN WAVE");
+    plt::save("xkcd.png");
+}
+
